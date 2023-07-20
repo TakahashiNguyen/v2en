68c705d2a1ba26d12d95c2bddf888c7d41e807be
takahashinguyen <vietuk.nguyen.git@icloud.com>
2023-07-20 22:52:28 +0700

    test: update todo.resolver.spec.ts

--- backend/src/todo/todo.resolver.spec.ts
+++ backend/src/todo/todo.resolver.spec.ts
@@ -1,18 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TodoResolver } from './todo.resolver';
+import { AppModule } from 'src/app.module';
 
 describe('TodoResolver', () => {
-  let resolver: TodoResolver;
+	let resolver: TodoResolver;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [TodoResolver],
-    }).compile();
+	beforeEach(async () => {
+		const module: TestingModule = await Test.createTestingModule({
+			imports: [AppModule],
+		}).compile();
 
-    resolver = module.get<TodoResolver>(TodoResolver);
-  });
+		resolver = module.get<TodoResolver>(TodoResolver);
+	});
 
-  it('should be defined', () => {
-    expect(resolver).toBeDefined();
-  });
+	it('should be defined', () => {
+		expect(resolver).toBeDefined();
+	});
 });
