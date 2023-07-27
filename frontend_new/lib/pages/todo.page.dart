aee3fb08e36381b6c5d82081c015178507c6d839
TakahashiNguyen <vietuk.nguyen.git@icloud.com>
2023-07-27 20:42:32 +0700

    update todo.page.dart

--- frontend_new/lib/pages/todo.page.dart
+++ frontend_new/lib/pages/todo.page.dart
@@ -25,36 +25,29 @@ class _TodoPageState extends State<TodoPage> {
   Widget build(BuildContext context) {
     newTodoTextController.text = '';
     return Scaffold(
-      body: Query(
-        options: todosQuery(),
-        builder: (result, {fetchMore, refetch}) {
-          return Scaffold(
-              body: result.hasException
-                  ? Column(
-                      children: [
-                        const Text('Something went wrong'),
-                        Text(result.exception.toString()),
-                      ],
-                    )
-                  : Column(
-                      children: [
-                        const Text('Todo List'),
-                        if (!result.isLoading)
-                          Column(
-                            children: [
+        body: Query(
+            options: todosQuery(),
+            builder: (result, {fetchMore, refetch}) {
+              return Scaffold(
+                  body: result.hasException
+                      ? Column(children: [
+                          const Text('Something went wrong'),
+                          Text(result.exception.toString())
+                        ])
+                      : Column(children: [
+                          const Text('Todo List'),
+                          if (!result.isLoading)
+                            Column(children: [
                               for (final todo in result.data?['todos'])
                                 TodoTag(
-                                  key: ValueKey(todo),
-                                  todo: todo['jobDescription'],
-                                ),
-                            ],
-                          )
-                        else
-                          const CircularProgressIndicator(),
-                        Form(
-                          child: Column(
-                            children: [
-                              TextFormField(
+                                    key: ValueKey(todo),
+                                    todo: todo['jobDescription'])
+                            ])
+                          else
+                            const CircularProgressIndicator(),
+                          Form(
+                              child: Column(children: [
+                            TextFormField(
                                 controller: newTodoTextController,
                                 decoration: const InputDecoration(
                                   labelText: 'Origin',
@@ -64,25 +57,18 @@ class _TodoPageState extends State<TodoPage> {
                                     return 'Please enter origin';
                                   }
                                   return null;
-                                },
-                              ),
-                              ElevatedButton(
+                                }),
+                            ElevatedButton(
                                 onPressed: () async {
                                   await widget.gqlCli.mutate(todoAddMutation(
                                       newTodoTextController.text));
                                   newTodoTextController.text = '';
                                   refetch!();
                                 },
-                                child: const Text('Add'),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ));
-        },
-      ),
-    );
+                                child: const Text('Add'))
+                          ]))
+                        ]));
+            }));
   }
 }
 
@@ -94,28 +80,19 @@ class TodoTag extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.all(8),
-      margin: const EdgeInsets.symmetric(vertical: 4),
-      decoration: BoxDecoration(
-        color: Colors.blue,
-        borderRadius: BorderRadius.circular(8),
-      ),
-      child: Row(
-        children: [
+        padding: const EdgeInsets.all(8),
+        margin: const EdgeInsets.symmetric(vertical: 4),
+        decoration: BoxDecoration(
+            color: Colors.blue, borderRadius: BorderRadius.circular(8)),
+        child: Row(children: [
           const Icon(
             Icons.check_circle,
             color: Colors.white,
           ),
           const SizedBox(width: 8),
-          Text(
-            todo,
-            style: const TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ],
-      ),
-    );
+          Text(todo,
+              style: const TextStyle(
+                  color: Colors.white, fontWeight: FontWeight.bold))
+        ]));
   }
 }
