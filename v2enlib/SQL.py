469b5c4726a6686b108b298b18b5ab40b52a1c9b
TakahashiNguyen <vietuk.nguyen.git@icloud.com>
2023-08-21 09:12:33 +0700

    update SQL.py

--- v2enlib/SQL.py
+++ v2enlib/SQL.py
@@ -10,7 +10,7 @@ scope = [
 ]
 creds = ServiceAccountCredentials.from_json_keyfile_name("client_secret.json", scope)
 client = gspread.authorize(creds)
-googleSheet = client.open("Moon'sVoiceDatabase")
+googleSheet = client.open("MyDatabase")
 
 
 def createOBJ(obj):
