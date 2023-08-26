c23be6e8c965dbd4314a55af08332ee38a0d43e3
takahashinguyen <vietuk.nguyen.git@icloud.com>
2023-08-26 10:42:10 +0700

    update 1 file and delete 1 file

--- addsent.py
+++ addsent.py
@@ -1,200 +1,14 @@
 from argparse import ArgumentParser
-from contextlib import suppress
-from multiprocessing import Process
-from multiprocessing.pool import Pool as mpPool
-import os, signal, time, gc
+from config import ExtraConfig, config as cconfig
+from v2enlib import utils, Language, Executor, InputSent, GSQLClass, Pool
+from multiprocessing import Manager, Process, current_process
 
-import v2enlib as v2l
-
-
-def signalHandler(sig, handle):
-    if v2l.config.main_execute:
-        print("\tStop program!")
-        v2l.config.main_execute = False
+import signal, os, time, gc
 
 
-def on_press(key):
-    with suppress(AttributeError):
-        if key.char == "z" and v2l.config.main_execute:
-            os.kill(os.getpid(), signal.SIGINT)
-
-
-class ExitButton:
-    def __init__(self, func, **kwargs):
-        if v2l.config.v2en.allow.GUI:
-            import tkinter as tk
-
-            self.root = tk.Tk()
-            self.root.title("My App")
-            self.button = tk.Button(self.root, text="Turn Off", command=self.turn_off)
-            self.button.pack()
-            self.thread = Process(
-                target=self.run_loop, kwargs={"func": func, "kwargs": kwargs}
-            )
-            self.thread.start()
-            self.root.mainloop()
-            self.thread.join()
-        else:
-            func(**kwargs)
-
-    def run_loop(self, func, kwargs):
-        func(**kwargs)
-
-        if v2l.config.v2en.allow.GUI and self.root.winfo_exists():
-            self.root.destroy()
-
-    def turn_off(self):
-        self.root.destroy()
-        if v2l.config.main_execute:
-            os.kill(os.getpid(), signal.SIGINT)
-
-
-def saveFiles(
-    saveIN,
-    saveOU,
-    first_dump_sents,
-    second_dump_sents,
-    flang,
-    slang,
-    fdictionary,
-    sdictionary,
-):
-    v2l.Language.saveDictionary(flang, fdictionary)
-    v2l.Language.saveDictionary(slang, sdictionary)
-
-    with open(v2l.config.v2en.first_path, "w") as file:
-        file.writelines(saveIN)
-    with open(v2l.config.v2en.second_path, "w") as file:
-        file.writelines(saveOU)
-
-    fsh = v2l.GSQLClass(v2l.config.v2en.sheet, f"dump_{flang}")
-    ssh = v2l.GSQLClass(v2l.config.v2en.sheet, f"dump_{slang}")
-    fsh.writeLRow([[e] for e in first_dump_sents])
-    ssh.writeLRow([[e] for e in second_dump_sents])
-    fsh.autoFit(), ssh.autoFit()
-
-
-def safeExecute(saveIN, saveOU, fdictionary, sdictionary, fargs):
-    false_count, first_dump_sents, second_dump_sents, exe_count, cmds = 0, [], [], 0, []
-    while v2l.config.main_execute:
-        # pre run section
-        time_start, pre_cmd = time.time(), len(cmds)
-        if v2l.utils.emptyFile(v2l.config.v2en.first_path) or v2l.utils.emptyFile(
-            v2l.config.v2en.second_path
-        ):
-            print("Done!")
-            break
-        first_dump_sent, second_dump_sent = [], []
-
-        # run section
-        for e in v2l.Pool.function(
-            v2l.Executor.addSent,
-            [
-                [
-                    v2l.InputSent(saveIN[idx], saveOU[idx]),
-                    fdictionary,
-                    sdictionary,
-                ]
-                for idx in range(
-                    v2l.config.v2en.num_sent
-                    if min(len(saveIN), len(saveOU)) > v2l.config.v2en.num_sent
-                    else min(len(saveIN), len(saveOU))
-                )
-            ],
-            mpPool,
-            strictOrder=True,
-            poolName="addSent",
-        ):
-            if e[0] != "" and e[1] != "":
-                first_dump_sent.append(e[0])
-                second_dump_sent.append(e[1])
-            cmds.extend(i for i in e[2] if i)
-            false_count += -false_count if e[3] else 1
-            fdictionary += e[4]
-            sdictionary += e[5]
-            if (
-                false_count > v2l.config.v2en.false_allow
-                and v2l.config.main_execute
-                and not v2l.config.v2en.allow.FalseTranslation
-            ):
-                v2l.utils.printError(
-                    "mainModule", Exception("Too many fatal translation!"), True
-                )
-                v2l.config.main_execute = False
-
-        # post run section
-        second_dump_sents += second_dump_sent
-        first_dump_sents += first_dump_sent
-        saveOU = saveOU[v2l.config.v2en.num_sent :]
-        saveIN = saveIN[v2l.config.v2en.num_sent :]
-        cmds = [list(x) for x in {tuple(x) for x in cmds}]
-
-        print(
-            f"\t\t(mainModule) time consume: {(time.time()-time_start):0,.2f} ({len(cmds)})",
-        )
-        del first_dump_sent, second_dump_sent
-        gc.collect()
-        exe_count += len(cmds) - pre_cmd
-        if exe_count >= fargs.amount_exe:
-            break
-
-    # save files
-    if v2l.config.main_execute:
-        sh = v2l.GSQLClass(v2l.config.v2en.sheet, v2l.config.v2en.worksheet)
-        sh.writeLRow(cmds), sh.autoFit()
-        saveFiles(
-            saveIN,
-            saveOU,
-            first_dump_sents,
-            second_dump_sents,
-            v2l.config.v2en.flang,
-            v2l.config.v2en.slang,
-            fdictionary,
-            sdictionary,
-        )
-
-
-def unsafeExecute(saveIN, saveOU, sql_connection, fdictionary, sdictionary):
-    false_count, first_dump_sents, second_dump_sents = 0, [], []
-
-
-def main(fargs):
-    # premain section
-    if not fargs.ci_cd:
-        v2l.utils.playNotes(*v2l.config.v2en.sound_tracks["macos_startup"])
-        listener = keyboard.Listener(on_press=on_press)
-        listener.start()
-    signal.signal(signal.SIGINT, signalHandler)
-
-    # init dictionaries
-    fdictionary = v2l.Language.loadDictionary(v2l.config.v2en.flang)
-    sdictionary = v2l.Language.loadDictionary(v2l.config.v2en.slang)
-    # init inputs
-    with open(v2l.config.v2en.first_path, "r") as first_file:
-        with open(v2l.config.v2en.second_path, "r") as second_file:
-            saveIN, saveOU = first_file.read().splitlines(
-                True
-            ), second_file.read().splitlines(True)
-
-    # main section
-    if v2l.config.v2en.safe_execute:
-        ExitButton(
-            safeExecute,
-            saveIN=saveIN,
-            saveOU=saveOU,
-            fdictionary=fdictionary,
-            sdictionary=sdictionary,
-            fargs=fargs,
-        )
-
-    # postmain section
-    if not fargs.ci_cd:
-        v2l.Sound.playNotes(*v2l.config.v2en.sound_tracks["windows7_shutdown"])
-
-
-if __name__ == "__main__":
-    # init program parse
+def initArgs(config: ExtraConfig):
     parser = ArgumentParser()
+
     parser.add_argument(
         "--amount-exe",
         type=int,
@@ -202,6 +16,7 @@ if __name__ == "__main__":
         nargs="?",
         default=0,
     )
+
     parser.add_argument(
         "--ci_cd",
         type=bool,
@@ -210,13 +25,15 @@ if __name__ == "__main__":
         default=False,
         const=True,
     )
+
     parser.add_argument(
         "--test",
         help="worksheet name to save output",
         nargs="?",
         const="test",
-        default=v2l.config.v2en.worksheet,
+        default=config.v2en.worksheet,
     )
+
     parser.add_argument(
         "--disable-thread",
         type=bool,
@@ -225,14 +42,162 @@ if __name__ == "__main__":
         default=False,
         const=True,
     )
-    fargs = parser.parse_args()
-    # reconfig program value
-    if fargs.disable_thread:
-        v2l.config.v2en.thread_alow = False
-    if not fargs.ci_cd:
-        from pynput import keyboard
-    else:
-        v2l.config.v2en.allow.tqdm = False
-    v2l.config.v2en.worksheet = fargs.test
-    # execute program
-    main(fargs)
+
+    return parser.parse_args()
+
+
+class Execute:
+    def __init__(self, func, config: ExtraConfig) -> None:
+        self.config = config
+        with Manager() as manager:
+            if config.v2en.allow.GUI:
+                import tkinter as tk
+
+                self.root = tk.Tk()
+                self.root.title("[v2en] AddSent.py")
+                self.button = tk.Button(self.root, text="Exit", command=self.exit)
+                self.button.pack()
+                self.thread = Process(target=self.loop, args=(func, manager))
+                self.thread.start()
+                self.root.mainloop()
+                self.thread.join()
+            else:
+                func(manager)
+
+    def loop(self, func, manger: Manager):
+        func(manger)
+
+        if self.config.v2en.allow.GUI and self.root.winfo_exists():
+            self.root.destroy()
+
+    def exit(self):
+        self.root.destroy()
+        if self.config.main_execute:
+            os.kill(os.getpid(), signal.SIGINT)
+
+
+class Main:
+    def __init__(self, args, config: ExtraConfig) -> None:
+        if args.disable_thread:
+            config.v2en.thread.allow = False
+        if args.ci_cd:
+            config.v2en.allow.tqdm = False
+        config.v2en.worksheet = args.test
+
+        self.args = args
+        self.config = config
+        self.cmds = []
+        self.main()
+
+    def signalHandler(self):
+        if self.config.main_execute:
+            print("Stop programme!")
+            self.config.main_execute = False
+
+    def handelEnvironment(self):
+        if not self.args.ci_cd:
+            utils.Sound.playNotes(*self.config.v2en.sound_tracks["macos_startup"])
+        signal.signal(signal.SIGINT, self.signalHandler)
+
+    def initValues(self):
+        self.fdictionary = Language.loadDictionary(
+            self.config.v2en.flang, self.config.v2en.sheet
+        )
+        self.sdictionary = Language.loadDictionary(
+            self.config.v2en.slang, self.config.v2en.sheet
+        )
+
+        with open(self.config.v2en.fpath, "r") as ffile:
+            self.fsent = ffile.read().splitlines(True)
+        with open(self.config.v2en.spath, "r") as sfile:
+            self.ssent = sfile.read().splitlines(True)
+        self.config.main_execute = self.fsent and self.ssent
+
+    def inputList(self, num_exe: int):
+        return [
+            [
+                InputSent(self.fsent[idx], self.ssent[idx]),
+                self.fdictionary,
+                self.sdictionary,
+                self.cmds,
+                self.config,
+            ]
+            for idx in range(num_exe)
+        ]
+
+    def mainExecute(self, manager: Manager):
+        self.fdictionary = manager.list(self.fdictionary)
+        self.sdictionary = manager.list(self.sdictionary)
+        self.cmds = manager.list(self.cmds)
+        false_count, fdump_sents, sdump_sents = 0, [], []
+        while self.config.main_execute and self.fsent and self.ssent:
+            time_start, pre_cmds = time.time(), len(self.cmds)
+
+            num_exe = min(len(self.fsent), len(self.ssent), self.config.v2en.num_sent)
+            for e in Pool.function(
+                func=Executor.addSent,
+                iterable=self.inputList(num_exe=num_exe),
+                force_pro=num_exe,
+            ):
+                if e[0] and e[1]:
+                    fdump_sents.append(e[0])
+                    sdump_sents.append(e[1])
+                false_count += -false_count if e[2] else 1
+                if (
+                    false_count > self.config.v2en.false_allow
+                    and self.config.main_execute
+                    and self.config.v2en.allow.FalseTranslation
+                ):
+                    utils.debuger.printError(
+                        self.mainExecute.__name__,
+                        Exception("Too many fatal traslation!"),
+                        True,
+                    )
+                    self.config.main_execute = False
+            self.fsent = self.fsent[self.config.v2en.num_sent :]
+            self.ssent = self.ssent[self.config.v2en.num_sent :]
+
+            gc.collect()
+            print(
+                f"\t\t({self.mainExecute.__name__}@{current_process().name}) time consume:\
+                {(time.time()-time_start):0,.2f} ({len(self.cmds)}) ({len(self.cmds)-pre_cmds})"
+            )
+            if len(self.cmds) >= self.args.amount_exe:
+                break
+        self.save(fdump=fdump_sents, sdump=sdump_sents)
+
+    def execute(self):
+        Execute(func=self.mainExecute, config=self.config)
+
+    def save(self, fdump: list, sdump: list):
+        if self.config.main_execute:
+            sh = GSQLClass(self.config.v2en.sheet, self.config.v2en.worksheet)
+            sh.writeLRow(self.cmds), sh.autoFit()
+
+            Language.saveDictionary(self.config.v2en.flang, self.fdictionary)
+            Language.saveDictionary(self.config.v2en.slang, self.sdictionary)
+
+            with open(self.config.v2en.fpath, "w") as f:
+                f.writelines(self.fsent)
+            with open(self.config.v2en.spath, "w") as f:
+                f.writelines(self.ssent)
+
+            for e in [[self.config.v2en.flang, fdump], [self.config.v2en.slang, sdump]]:
+                with GSQLClass(self.config.v2en.sheet, f"dump_{e[0]}") as sheet:
+                    sheet.writeLRow([[e] for e in e[1]])
+                    sheet.autoFit()
+
+    def exit(self):
+        if not self.args.ci_cd:
+            utils.Sound.playNotes(*self.config.v2en.sound_tracks["windows7_shutdown"])
+
+    def main(self) -> None:
+        self.handelEnvironment()
+        self.initValues()
+        self.execute()
+        self.exit()
+
+
+if __name__ == "__main__":
+    args = initArgs(config=cconfig)
+    main = Main(args=args, config=cconfig)
