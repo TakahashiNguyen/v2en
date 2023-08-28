6f613ddb4fe3e65dfecd393ae8454631229b015d
TakahashiNguyen <vietuk.nguyen.git@icloud.com>
2023-08-28 09:38:23 +0700

    delete 9 files

--- .sh/build.sh
+++
@@ -1,3 +0,0 @@
-cat /home/hayasaka/.ssh/id_rsa.pub >> id_rsa.pub
-docker build --build-arg user=$USER -t nvietuk/v2en:latest .
-rm id_rsa.pub
--- .sh/cont.sh
+++
@@ -1,1 +0,0 @@
-docker run --gpus all -it -v .:/v2en --cap-add=NET_ADMIN --network="bridge" -p 2564:2564 -e TZ=Asia/Ho_Chi_Minh -m 8G nvietuk/v2en:latest
